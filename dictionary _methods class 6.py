cdb421cb7f350bdf8775ff2f99163b64bc25845e
Umadevi_veluduti <101104398+umadevi897@users.noreply.github.com>
2024-08-05 16:01:27 +0530

    Add files via upload
    Task 16 dictionary,tuple and ATM program 
    task 17 document 
    task 18 1st 10 interview quns completed

---
+++ dictionary _methods class 6.py
@@ -0,0 +1,115 @@
+
+# class 6
+
+
+# dictionary methods
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# print(aswini.get("sno"))
+
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# print(aswini.keys())
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# print(aswini.values())
+
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# print(aswini.items())
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# aswini.update({"food":"biryani"})
+# print(aswini)
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# aswini.pop("sno")
+# print(aswini)
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# aswini.pop("name")
+# print(aswini)
+
+
+# aswini={"sno":1,"name":"sunny","phone":[12,32]}
+# for bawarchi in aswini:
+# print(bawarchi)
+
+
+
+# tuple
+
+
+# mohan=(1,22,2,232,33)
+# print(type(mohan))
+
+
+# mohan=(1,22,2,232,33)
+# print(type(0))
+
+
+# mohan=(1,22,2,232,33)
+# print (mohan[])
+# mohan[0]="kiran"
+# print(mohan)
+
+# mohan=(1,22,2,232,33)
+# print(max(mohan))
+# print(min(mohan))
+# print(sum(mohan))
+# print(len(mohan))
+
+
+# mohan=(1,22,2,232,33)
+# print(list(mohan))
+
+
+
+
+
+
+
+
+
+
+# dictionary with in a dictionary
+
+# phani={
+#     1:"a",
+#     2:"b",
+#     3:{1:"aa"}
+# }
+# print(phani)
+
+
+# phani={
+#     1:"a",
+#     2:"b",
+#     3:{1:"aa"}
+# }
+# print(phani[3][1])
+
+# concatination
+
+# t1=(1,2,3)
+# t2=(4,5,6)
+# print(t1+t2)
+
+# t1=(1,2,3)
+# t2=(4,5,6)
+# s=zip(t1+t2)
+# print(tuple(s))
+
+
+# t1=(1,2,3)
+# t2=(4,5,6)
+# new=[]
+# for i,j in zip(t1+t2):
+#     new.append[i+j]
+# print(tuple[new])
+
+
+# d=(1,2,3)
+# print(d*10)
+
+
